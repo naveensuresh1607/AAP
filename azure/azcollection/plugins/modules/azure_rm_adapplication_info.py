283361d54cd6a989952104b783d46fd4da1647a8
root <root@rhaap.server.in>
2025-07-16 15:55:07 +0530

    collection_update

---
+++ azure/azcollection/plugins/modules/azure_rm_adapplication_info.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2020 Guopeng Lin, <linguopeng1998@google.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = '''
+module: azure_rm_adapplication_info
+
+version_added: "1.6.0"
+
+short_description: Get Azure Active Directory application info
+
+description:
+    - Get Azure Active Directory application info.
+
+options:
+    app_id:
+        description:
+            - The application ID.
+        type: str
+    tenant:
+        description:
+            - (deprecated) The tenant ID.
+            - This option has been deprecated, and will be removed in the future.
+        type: str
+    object_id:
+        description:
+            - It's application's object ID.
+        type: str
+    identifier_uri:
+        description:
+            - It's identifier_uri's object ID.
+        type: str
+
+extends_documentation_fragment:
+    - azure.azcollection.azure
+
+author:
+    haiyuan_zhang (@haiyuazhang)
+    Fred-sun (@Fred-sun)
+    guopeng_lin (@guopenglin)
+    Xu Zhang (@xuzhang)
+'''
+
+EXAMPLES = '''
+- name: get ad app info by App ID
+  azure_rm_adapplication_info:
+    app_id: "{{ app_id }}"
+
+- name: get ad app info ---- by object ID
+  azure_rm_adapplication_info:
+    object_id: "{{ object_id }}"
+
+- name: get ad app info ---- by identifier uri
+  azure_rm_adapplication_info:
+    identifier_uri: "{{ identifier_uri }}"
+'''
+
+RETURN = '''
+applications:
+    description:
+        - The info of the ad application.
+    type: complex
+    returned: aways
+    contains:
+        app_display_name:
+            description:
+                - Object's display name or its prefix.
+            type: str
+            returned: always
+            sample: app
+        app_id:
+            description:
+                - The application ID.
+            returned: always
+            type: str
+            sample: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        identifier_uris:
+            description:
+                - The identifiers_uri list of app.
+            type: list
+            returned: always
+            sample: ["http://ansible-atodorov"]
+        object_id:
+            description:
+                - It's application's object ID.
+            returned: always
+            type: str
+            sample: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+'''
+
+from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common_ext import AzureRMModuleBase
+
+try:
+    import asyncio
+    from msgraph.generated.applications.applications_request_builder import ApplicationsRequestBuilder
+    from kiota_abstractions.api_error import APIError
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+
+class AzureRMADApplicationInfo(AzureRMModuleBase):
+
+    def __init__(self):
+        self.module_arg_spec = dict(
+            app_id=dict(type='str'),
+            object_id=dict(type='str'),
+            identifier_uri=dict(type='str'),
+            tenant=dict(type='str')
+        )
+        self.tenant = None
+        self.app_id = None
+        self.object_id = None
+        self.identifier_uri = None
+        self.results = dict(changed=False)
+        self._client = None
+        super(AzureRMADApplicationInfo, self).__init__(derived_arg_spec=self.module_arg_spec,
+                                                       supports_check_mode=True,
+                                                       supports_tags=False,
+                                                       is_ad_resource=True)
+
+    def exec_module(self, **kwargs):
+        for key in list(self.module_arg_spec.keys()):
+            setattr(self, key, kwargs[key])
+
+        applications = []
+
+        if self.tenant:
+            self.deprecate('tenant ID has been deprecated and will be removed in the future. See the Azure documentation for more information: '
+                           'https://learn.microsoft.com/en-us/graph/migrate-azure-ad-graph-request-differences#example-request-comparison',
+                           version='2.0.0',
+                           collection_name='azure.azcollection')
+
+        try:
+            self._client = self.get_msgraph_client()
+            if self.object_id:
+                applications = [asyncio.get_event_loop().run_until_complete(self.get_application(self.object_id))]
+            else:
+                sub_filters = []
+                if self.identifier_uri:
+                    sub_filters.append("identifierUris/any(s:s eq '{0}')".format(self.identifier_uri))
+                if self.app_id:
+                    sub_filters.append("appId eq '{0}'".format(self.app_id))
+
+                apps = asyncio.get_event_loop().run_until_complete(self.get_applications(sub_filters))
+                applications = list(apps.value)
+            self.results['applications'] = [self.to_dict(app) for app in applications]
+        except APIError as e:
+            if e.response_status_code != 404:
+                self.fail("failed to get application info {0}".format(str(e)))
+        except Exception as ge:
+            self.fail("failed to get application info {0}".format(str(ge)))
+
+        return self.results
+
+    def to_dict(self, object):
+        return dict(
+            app_id=object.app_id,
+            object_id=object.id,
+            app_display_name=object.display_name,
+            identifier_uris=object.identifier_uris
+        )
+
+    async def get_application(self, obj_id):
+        return await self._client.applications.by_application_id(obj_id).get()
+
+    async def get_applications(self, sub_filters):
+        if sub_filters:
+            request_configuration = ApplicationsRequestBuilder.ApplicationsRequestBuilderGetRequestConfiguration(
+                query_parameters=ApplicationsRequestBuilder.ApplicationsRequestBuilderGetQueryParameters(
+                    filter=(' and '.join(sub_filters)),
+                ),
+                headers={'ConsistencyLevel': "eventual"},
+            )
+            return await self._client.applications.get(request_configuration=request_configuration)
+        else:
+            return await self._client.applications.get()
+
+
+def main():
+    AzureRMADApplicationInfo()
+
+
+if __name__ == '__main__':
+    main()
---
+++ azure/azcollection/plugins/modules/azure_rm_adserviceprincipal_info.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2020 Haiyuan Zhang, <haiyzhan@microsoft.com>
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = '''
+module: azure_rm_adserviceprincipal_info
+
+version_added: "0.2.0"
+
+short_description: Get Azure Active Directory service principal info
+
+description:
+    - Get Azure Active Directory service principal info.
+
+options:
+    app_id:
+        description:
+            - The application ID.
+        type: str
+    tenant:
+        description:
+            - (deprecated) The tenant ID.
+            - This option has been deprecated, and will be removed in the future.
+        type: str
+    object_id:
+        description:
+            - It's service principal's object ID.
+        type: str
+
+extends_documentation_fragment:
+    - azure.azcollection.azure
+
+author:
+    haiyuan_zhang (@haiyuazhang)
+    Fred-sun (@Fred-sun)
+'''
+
+EXAMPLES = '''
+- name: get ad sp info
+  azure_rm_adserviceprincipal_info:
+    app_id: "{{ app_id }}"
+'''
+
+RETURN = '''
+app_display_name:
+    description:
+        - Object's display name or its prefix.
+    type: str
+    returned: always
+    sample: sp
+app_id:
+    description:
+        - The application ID.
+    returned: always
+    type: str
+    sample: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+app_role_assignment_required:
+    description:
+        - Whether the Role of the Service Principal is set.
+    type: bool
+    returned: always
+    sample: false
+object_id:
+    description:
+        - It's service principal's object ID.
+    returned: always
+    type: str
+    sample: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+
+'''
+
+from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common_ext import AzureRMModuleBase
+
+try:
+    import asyncio
+    from msgraph.generated.service_principals.service_principals_request_builder import ServicePrincipalsRequestBuilder
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+
+class AzureRMADServicePrincipalInfo(AzureRMModuleBase):
+    def __init__(self):
+
+        self.module_arg_spec = dict(
+            app_id=dict(type='str'),
+            object_id=dict(type='str'),
+            tenant=dict(type='str'),
+        )
+
+        self.tenant = None
+        self.app_id = None
+        self.object_id = None
+        self.results = dict(changed=False)
+
+        super(AzureRMADServicePrincipalInfo, self).__init__(derived_arg_spec=self.module_arg_spec,
+                                                            supports_check_mode=True,
+                                                            supports_tags=False,
+                                                            is_ad_resource=True)
+
+    def exec_module(self, **kwargs):
+
+        for key in list(self.module_arg_spec.keys()):
+            setattr(self, key, kwargs[key])
+
+        if self.tenant:
+            self.deprecate('tenant ID has been deprecated and will be removed in the future. See the Azure documentation for more information: '
+                           'https://learn.microsoft.com/en-us/graph/migrate-azure-ad-graph-request-differences#example-request-comparison',
+                           version='2.0.0',
+                           collection_name='azure.azcollection')
+
+        self._client = self.get_msgraph_client()
+
+        service_principals = []
+
+        try:
+            if self.object_id is None:
+                sps = asyncio.get_event_loop().run_until_complete(self.get_service_principals())
+                service_principals = list(sps.value)
+            else:
+                service_principals = [asyncio.get_event_loop().run_until_complete(self.get_service_principal())]
+
+            self.results['service_principals'] = [self.to_dict(sp) for sp in service_principals]
+        except Exception as ge:
+            self.fail("failed to get service principal info {0}".format(str(ge)))
+
+        return self.results
+
+    def to_dict(self, object):
+        return dict(
+            app_id=object.app_id,
+            object_id=object.id,
+            app_display_name=object.display_name,
+            app_role_assignment_required=object.app_role_assignment_required
+        )
+
+    async def get_service_principal(self):
+        return await self._client.service_principals.by_service_principal_id(self.object_id).get()
+
+    async def get_service_principals(self):
+        request_configuration = ServicePrincipalsRequestBuilder.ServicePrincipalsRequestBuilderGetRequestConfiguration(
+            query_parameters=ServicePrincipalsRequestBuilder.ServicePrincipalsRequestBuilderGetQueryParameters(
+                filter="servicePrincipalNames/any(c:c eq '{0}')".format(self.app_id),
+            ),
+        )
+        return await self._client.service_principals.get(request_configuration=request_configuration)
+
+
+def main():
+    AzureRMADServicePrincipalInfo()
+
+
+if __name__ == '__main__':
+    main()
